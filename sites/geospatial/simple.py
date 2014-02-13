f98c7f3a67a48a147929bb174016fc92c95dee4d
Dan Catalano <daniel.w.catalano@gmail.com>
2014-02-12 18:04:49 -0800

    updated flask project folder and location
    folder locally now called geospatial for geospatial website
    
    folder on VM which corresponds with above folder is located
    /var/www/sites/geospatial
    
    Issue #2

--- sites/geospatial/simple.py
+++ sites/geospatial/simple.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def simple_app():
-    return 'Simple statement of success!'
+    return 'Lala simple different statement of success!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
