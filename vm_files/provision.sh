6933c35374a87c859c2539f118e790f18d85daf1
Dan Catalano <daniel.w.catalano@gmail.com>
2014-02-17 17:49:18 -0800

    configuring apache to with with flask
    creating virtual host for apache
    
    renamed simple flask app to something more appropriate
    
    Vagrantfile now fowards host 8080 to client 80
    
    gitignore updated to ignore crumb left by vagrant
    
    provision updated to install and configure apache
    
    Issue #7

--- vm_files/provision.sh
+++ vm_files/provision.sh
@@ -13,6 +13,12 @@ fi
 # update machine before installing required applications
 apt-get update
 apt-get upgrade -y
-apt-get install -y python-dev python-pip byobu
+apt-get install -y python-dev python-pip byobu apache2 libapache2-mod-wsgi
 
 pip install flask
+
+a2dissite default
+chmod -R a+rx /var/www/pedalpdx.com
+ln -s /vagrant/pedalpdx.com.conf /etc/apache2/sites-available/pedalpdx.com.conf
+a2ensite pedalpdx.com.conf
+service apache2 restart
